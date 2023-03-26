7a2bbcc2df8ee4fe59bfbeb2338012f8c0893f1a
DevFalkon <gitfalkon@gmail.com>
2023-03-26 15:06:12 +0530

    Update __init__.py

--- modules/PyMusic/__init__.py
+++ modules/PyMusic/__init__.py
@@ -1,12 +1,28 @@
 import subprocess
+import time
 import urllib.request
 import re
 import os
 import pygame as pg
-from pytube import YouTube
+from difflib import SequenceMatcher
 
 
 video_id = []
+sng_info = {}
+
+
+def get_name(title, query):
+    if ' - ' in title:
+        title = title.split(' - ')
+    if type(title) == list:
+        if '(' in title[1]:
+            title[1] = title[1].split('(')[0]
+            return title[0], title[1]
+    return None
+
+
+def match_query(name, query):
+    return SequenceMatcher(None, name, query).ratio()
 
 
 def web_scraper(query):
@@ -29,8 +45,16 @@ def web_scraper(query):
     video_id = vid_id
     for i in vid_id:
         try:
-            vid = YouTube(f"https://www.youtube.com/watch?v={i}")
-            name = vid.title
+            process = subprocess.Popen(['utilities\\yt-dlp\\yt-dlp.exe', '--print', 'title', i],
+                                       stdout=subprocess.PIPE)
+            name = str(process.communicate()[0])[2:-3]
+            res = get_name(name, query)
+            if res:
+                artist, title = res
+                if match_query(title.lower(), query) > 0.7:
+                    sng_info[title] = artist
+                    print(sng_info)
+                    break
         except Exception:
             continue
 
@@ -43,15 +67,30 @@ curr_ints = False
 def inst(link, name):
     global is_temp_playing, temp_sng, curr_ints
     curr_ints = True
-    subprocess.call(f'utilities\\yt-dlp\\yt-dlp.exe -o \"saved\\temp\\{name}.mp4\" {link}',
-                    creationflags=subprocess.CREATE_NO_WINDOW)
+    try:
+        subprocess.call(f'utilities\\yt-dlp\\yt-dlp.exe -o \"saved\\temp\\{name}.mp4\" {link}',
+                        creationflags=subprocess.CREATE_NO_WINDOW, timeout=10)
+    except subprocess.TimeoutExpired:
+        print('yt-dlp timeout')
+        curr_ints = False
+        return
 
     if name == temp_sng:
         pg.mixer.music.unload()
         os.remove(f"saved\\temp\\{name}.wav")
-    subprocess.call(f'utilities\\ffmpeg\\ffmpeg.exe -i "saved\\temp\\{name}.mp4" "saved\\temp\\{name}.wav"',
-                    creationflags=subprocess.CREATE_NO_WINDOW)
-    os.remove(f"saved\\temp\\{name}.mp4")
-    is_temp_playing = True
-    temp_sng = name
-    curr_ints = False
+
+    try:
+        if f'{name}.wav' in os.listdir("saved\\temp"):
+            os.remove(f"saved\\temp\\{name}.wav")
+        subprocess.call(f'utilities\\ffmpeg\\ffmpeg.exe -i "saved\\temp\\{name}.mp4" "saved\\temp\\{name}.wav"',
+                        creationflags=subprocess.CREATE_NO_WINDOW, timeout=5)
+        os.remove(f"saved\\temp\\{name}.mp4")
+        is_temp_playing = True
+        temp_sng = name
+        curr_ints = False
+    except subprocess.TimeoutExpired:
+        curr_ints = False
+        print('ffmpeg timeout')
+        if f'{name}.mp4' in os.listdir("saved\\temp"):
+            os.remove(f"saved\\temp\\{name}.mp4")
+        return
