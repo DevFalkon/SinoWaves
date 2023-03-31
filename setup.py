9f5fe81614c2c34a964eb32e0a9dd6001b2a1274
DevFalkon <gitfalkon@gmail.com>
2023-03-31 16:19:28 +0530

    auto generate version.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
 from sys import argv
+import urllib.request
+import re
 
 print('setting up virtual python environment')
 subprocess.run("python -m venv env", shell=True)
@@ -17,5 +19,10 @@ subprocess.run("rmdir /s /Q build", shell=True)
 subprocess.run("rmdir /s /Q dist", shell=True)
 if "del" in os.listdir():
     subprocess.run("rmdir /s /Q del", shell=True)
+with open('version.txt', 'w') as file:
+    html = urllib.request.urlopen("https://github.com/DevFalkon/SinoWaves/releases")
+    versions = re.findall(r"href=\"/DevFalkon/SinoWaves/releases/tag/v(\S{5})\"", html.read().decode())
+    highest_version = versions[0]
+    file.write(f"v{highest_version}")
 os.remove(argv[0])
 subprocess.Popen(['SinoWaves.exe'])
