d1cecd20beb11d192fe0365e10c12c28c03de722
DevFalkon <gitfalkon@gmail.com>
2023-04-08 14:46:22 +0530

    manual window resize

--- modules/PygameGUI/__init__.py
+++ modules/PygameGUI/__init__.py
@@ -94,6 +94,10 @@ class GuiWindow:
         self.last_position = self.window.position
         self.last_size = self.window.size
         self.is_maximised = False
+        self.left_resize_lock = False
+        self.bottom_resize_lock = False
+        self.right_resize_lock = False
+        self.initial_dimension = self.width, self.height
 
     # Run this method in every iteration of the main loop
     def win_update(self):
@@ -126,7 +130,27 @@ class GuiWindow:
         if self.is_out_of_screen and self.window.position[0] > 0 and \
                 self.window.size[0] + self.window.position[0] < get_max_window()[0]:
             self.is_out_of_screen = False
+            pg.display.flip()
+            self.title_bar()
             self.screen_update = True
+        if left_click and mouse_pos[1] > self.top_bar_height:
+            if self.drag_lock and not self.is_maximised:
+                if self.width >= 1000 and self.height >= 700:
+                    self.manual_resize(mouse_pos)
+                if self.left_resize_lock or self.bottom_resize_lock or self.right_resize_lock:
+                    if self.width < 1000:
+                        self.width = 1000
+                        self.window.size = self.width, self.height
+                    if self.height < 700:
+                        self.height = 700
+                        self.window.size = self.width, self.height
+                    self.screen_update = True
+        else:
+            self.initial_dimension = self.width, self.height
+            if not pg.mouse.get_pressed()[0]:
+                self.left_resize_lock = False
+                self.bottom_resize_lock = False
+                self.right_resize_lock = False
 
     def drag_window(self, mouse_pos, click):
         mouse_x, mouse_y = mouse_pos
@@ -153,9 +177,11 @@ class GuiWindow:
 
                 if not self.is_maximised:
                     pos_x = self.initial_window_position[0] - \
-                        (self.initial_window_position[0] - get_abs_cursor_pos()[0]) - self.relative_mouse_position[0]
+                            (self.initial_window_position[0] - get_abs_cursor_pos()[0]) - self.relative_mouse_position[
+                                0]
                     pos_y = self.initial_window_position[1] - \
-                        (self.initial_window_position[1] - get_abs_cursor_pos()[1]) - self.relative_mouse_position[1]
+                            (self.initial_window_position[1] - get_abs_cursor_pos()[1]) - self.relative_mouse_position[
+                                1]
 
                     if pos_y < -15:
                         pos_y = -15
@@ -163,7 +189,7 @@ class GuiWindow:
 
                 elif mouse_x <= self.width - 90:  # Minimising the window if dragged in full screen mode
                     self.minimise()
-                    self.window.position = get_abs_cursor_pos()[0] - self.width//2, 0
+                    self.window.position = get_abs_cursor_pos()[0] - self.width // 2, 0
                     self.initial_window_position = self.window.position
                     self.drag_lock = True
                     self.was_maximised = True
@@ -172,7 +198,6 @@ class GuiWindow:
             if click:
                 self.drag_lock = True
 
-    @staticmethod
     def centre_window(self):  # To centre the pygame window
         max_width, max_height = get_max_window()
         pos_x = max_width // 2 - self.width // 2
@@ -182,6 +207,7 @@ class GuiWindow:
     def maximise(self):
         self.screen_update = True
         self.last_position = self.window.position
+        self.last_size = self.width, self.height
         self.is_maximised = True
 
         self.width, self.height = get_max_window()
@@ -200,6 +226,49 @@ class GuiWindow:
         pg.display.flip()
         self.title_bar()
 
+    def manual_resize(self, mouse_pos):
+        mouse_x, mouse_y = mouse_pos
+        border = 10
+        if mouse_x <= border or self.left_resize_lock:
+            if not (self.right_resize_lock or self.bottom_resize_lock):
+                if not self.left_resize_lock:
+                    self.initial_mouse_position = get_abs_cursor_pos()
+                    self.left_resize_lock = True
+                else:
+                    final_pos = get_abs_cursor_pos()
+                    width_add = final_pos[0] - self.initial_mouse_position[0]
+                    self.width, self.height = self.initial_dimension[0] - width_add, self.height
+                    self.window.position = self.initial_window_position[0] + width_add, self.initial_window_position[1]
+                    self.window.size = self.width, self.height
+                    pg.display.flip()
+                    self.title_bar()
+
+        if self.height - border <= mouse_y or self.bottom_resize_lock:
+            if not (self.left_resize_lock or self.right_resize_lock):
+                if not self.bottom_resize_lock:
+                    self.initial_mouse_position = get_abs_cursor_pos()
+                    self.bottom_resize_lock = True
+                else:
+                    final_pos = get_abs_cursor_pos()
+                    height_add = final_pos[1] - self.initial_mouse_position[1]
+                    self.width, self.height = self.width, self.initial_dimension[1] + height_add
+                    self.window.size = self.width, self.height
+                    pg.display.flip()
+                    self.title_bar()
+
+        if self.width - border <= mouse_x or self.right_resize_lock:
+            if not (self.left_resize_lock or self.bottom_resize_lock):
+                if not self.right_resize_lock:
+                    self.initial_mouse_position = get_abs_cursor_pos()
+                    self.right_resize_lock = True
+                else:
+                    final_pos = get_abs_cursor_pos()
+                    width_add = final_pos[0] - self.initial_mouse_position[0]
+                    self.width, self.height = self.initial_dimension[0] + width_add, self.height
+                    self.window.size = self.width, self.height
+                    pg.display.flip()
+                    self.title_bar()
+
     def title_bar(self, color='grey'):
         title_bar_color = self.color(color)
         pg.draw.rect(self.screen, title_bar_color, pg.Rect(0, 0, self.width, self.top_bar_height))
@@ -209,11 +278,11 @@ class GuiWindow:
             font = pg.font.Font('modules\\PygameGUI\\fonts\\Inter-Regular.ttf', title_size)
             text = font.render(self.title, True, self.color('white'))
             text_width = text.get_width()
-            self.screen.blit(text, (self.width // 2 - text_width // 2 - 2, 5))
+            self.screen.blit(text, (self.width // 2 - text_width // 2, 5))
 
         if self.logo:
             icon = pg.image.load(self.logo).convert_alpha()
-            icon = pg.transform.smoothscale(icon, (self.top_bar_height-5, self.top_bar_height-5))
+            icon = pg.transform.smoothscale(icon, (self.top_bar_height - 5, self.top_bar_height - 5))
             self.screen.blit(icon, (10, 2))
 
         self.close_bttn = Circle(self.screen, self.width - 25,
@@ -245,9 +314,6 @@ class GuiWindow:
         return colors[color]
 
     def quit_app(self):
-        pg.mixer.music.unload()
-        for file in os.listdir('saved\\temp'):
-            os.remove(f'saved\\temp\\{file}')
         # Closing animation
         opacity = self.window.opacity * 100
         while opacity > 0:
